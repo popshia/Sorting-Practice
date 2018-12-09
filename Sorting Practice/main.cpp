7d1dcdb273d8067b3b12166c865d207f855f3cef
popshia <B123292179@gmail.com>
2018-12-09 14:12:50 +0800

    uploaded version

--- Sorting Practice/main.cpp
+++ Sorting Practice/main.cpp
@@ -15,10 +15,10 @@
 using namespace std ;
 
 typedef struct DataStruct {
-    string schoolNum = "\0" ;
-    string schoolName = "\0" ;
-    string departmentNum = "\0" ;
-    string departmentName = "\0" ;
+    //string schoolNum = "\0" ;
+    //string schoolName = "\0" ;
+    //string departmentNum = "\0" ;
+    //string departmentName = "\0" ;
     int student = 0 ;
     int teacher = 0 ;
     int graduated = 0 ;
@@ -33,7 +33,7 @@ static int Count = 0 ;
 class DestroyDickDecember {
     vector<DataStruct> dataBase ;
     vector<DataStruct> sorted ;
-    
+
 public:
     void inputData() { // tool
         dataBase.clear() ;
@@ -43,29 +43,29 @@ public:
         getline( input, sentence ) ;
         getline( input, sentence ) ;
         getline( input, sentence ) ;
-        
+
         while ( getline( input, sentence ) ) {
             // cout << sentence << endl ;
             tempData.wholeSentence = sentence ;
             vector<string> cut ;
             string token ;
             istringstream cutStream( sentence ) ;
-            
+
             while ( getline( cutStream, token, '\t' ) ) // cut the token one by one
                 cut.push_back( token ) ;
-            
-            tempData.schoolNum = cut[0] ;
-            tempData.schoolName = cut[1] ;
-            tempData.departmentNum = cut[2] ;
-            tempData.departmentName = cut[3] ;
-            
+
+            //tempData.schoolNum = cut[0] ;
+            //tempData.schoolName = cut[1] ;
+            //tempData.departmentNum = cut[2] ;
+            //tempData.departmentName = cut[3] ;
+
             if ( cut[6].size() > 3 ) {
                 cut[6].erase( find( cut[6].begin(), cut[6].end(), '"' ) ) ;
                 cut[6].erase( find( cut[6].begin(), cut[6].end(), ',' ) ) ;
                 cut[6].erase( find( cut[6].begin(), cut[6].end(), '"' ) ) ;
             } // erase '"' & ','
             tempData.student = atoi( cut[6].c_str() ) ;
-            
+
             if ( cut[8].size() > 3 ) {
                 cut[8].erase( find( cut[8].begin(), cut[8].end(), '"' ) ) ;
                 cut[8].erase( find( cut[8].begin(), cut[8].end(), ',' ) ) ;
@@ -76,7 +76,7 @@ public:
             Count++ ;
         } // get the whole file
     } // input the data
-    
+
     int FindLargest() { // tool
         int largestIndex = 0 ;
         for ( int i = 0 ; i < dataBase.size() ; i++ ) {
@@ -84,7 +84,7 @@ public:
         }
         return largestIndex ;
     } // find the largest
-    
+
     int FindSmallest() { // tool
         int smallestIndex = 0 ;
         for ( int i = 0 ; i < dataBase.size() ; i++ ) {
@@ -92,7 +92,7 @@ public:
         }
         return smallestIndex ;
     } // find the smallest
-    
+
     void SelectionSort() { // done
         while ( ! dataBase.empty() ) {
             int largestIndex = FindLargest() ;
@@ -101,7 +101,7 @@ public:
             dataBase.erase( dataBase.begin()+largestIndex ) ;
         } // add to new vector and erase
     } // function 1: Selection Sort (done)
-    
+
     void BubbleSort() { // done
         for ( int one = 0 ; one < dataBase.size() ; one++ ) {
             // cout << "one: " << dataBase[one].graduated << endl ;
@@ -112,19 +112,19 @@ public:
         } // for
         for ( int i = 0 ; i < dataBase.size() ; i++ ) output << dataBase[i].wholeSentence << endl ;
     } // function 1: Bubble Sort (done)
-    
+
     void Merge( int front, int mid, int end ) { // done
         vector<DataStruct> left( dataBase.begin()+front, dataBase.begin()+mid+1 ) ;
         vector<DataStruct> right( dataBase.begin()+mid+1, dataBase.begin()+end+1 ) ;
         DataStruct max ;
         max.graduated = 99999 ;
-        
+
         left.push_back( max ) ;
         right.push_back( max ) ;
-        
+
         int leftIndex = 0 ;
         int rightIndex = 0 ;
-        
+
         for ( int i = front ; i <= end ; i++ ) {
             // cout << left[leftIndex].graduated << " " << right[rightIndex].graduated << endl ;
             if ( left[leftIndex].graduated <= right[rightIndex].graduated ) {
@@ -137,7 +137,7 @@ public:
             } // right
         } // compare two sub vectors
     } // merge left and right sub vectors
-    
+
     void MergeSort( int front, int end ) { // done
         if ( front < end ) {
             int mid = ( front + end ) / 2 ;
@@ -145,47 +145,49 @@ public:
             MergeSort( mid+1, end ) ;
             Merge( front, mid, end ) ;
         } // run recursive
-        
+
         sorted = dataBase ;
         reverse( sorted.begin(), sorted.end() ) ;
     } // function 2: Merge Sort
-    
+
     int Partition( int front, int end ) { // done
         int pivot = dataBase[end].graduated ;
         int i = front - 1 ;
-        
+
         for ( int j = front ; j < end ; j++ ) {
             if ( dataBase[j].graduated < pivot ) {
                 i++ ;
                 swap( dataBase[i], dataBase[j] ) ;
             } // if smaller, swap
         } // run the whole data
-        
+
         i++ ;
         swap( dataBase[i], dataBase[end] ) ;
         return i ;
     } // Partition
-    
+
     void QuickSort( int front, int end ) { // done
         if ( front < end ) {
             int pivot = Partition( front, end ) ;
             QuickSort( front, pivot-1 ) ;
             QuickSort( pivot+1, end ) ;
         } // run recursive
-        
+
         /*cout << "Sorted: " ;
          for ( int i = 0 ; i < dataBase.size() ; i++ ) cout << dataBase[i].graduated << " " ;
          cout << endl ;*/
         sorted = dataBase ;
         reverse( sorted.begin(), sorted.end() ) ;
     } // function 2: Quick Sort
-    
+
     void RadixSort() { // done
-        DataStruct temp[Count][Count] ;
+        // cout << Count << endl ;
+        // cout << "here" << endl ;
+        DataStruct temp[10][Count] ; // stack overflow, but dont know why, runs perfectly fine in xcode, crashes in codeblocks
         int order[Count] ;
         for ( int i = 0 ; i < Count ; i ++ ) order[i] = 0 ;
         int n = 1 ;
-        
+
         while ( n <= dataBase[FindLargest()].graduated ) {
             int i ;
             for ( i = 0 ; i < Count ; i++ ) {
@@ -193,26 +195,26 @@ public:
                 temp[lsd][order[lsd]] = dataBase[i] ;
                 order[lsd]++ ;
             } // first category
-            
+
             int k = 0 ;
-            for ( i = 0 ; i < Count ; i++ ) {
+            for ( i = 10 ; i >= 0 ; i-- ) {
                 if ( order[i] != 0 ) {
                     int j ;
                     for ( j = 0 ; j < order[i] ; j++, k++ ) dataBase[k] = temp[i][j] ;
                 } // get the one out
                 order[i] = 0 ;
             } // reorder
-            
+
             n *= 10 ;
         } // run the whole data
-        
+
         sorted = dataBase ;
-        reverse( sorted.begin(), sorted.end() ) ;
+        // reverse( sorted.begin(), sorted.end() ) ;
         /*cout << "Sorted: " ;
          for ( int i = 0 ; i < dataBase.size() ; i++ ) cout << dataBase[i].graduated << " " ;
          cout << endl ;*/
     } // function 3: Radix Sort
-    
+
     void Print() { for( int i = 0 ; i < sorted.size() ; i++ ) output << sorted[i].wholeSentence << endl ; } // Print out sorted data
 } ; // DestroyDickDecember
 
@@ -222,7 +224,7 @@ int main() {
     DestroyDickDecember function1 ;
     DestroyDickDecember function2 ;
     DestroyDickDecember function3 ;
-    
+
     do {
         cout << "*****************************************" << endl ; // welcome message
         cout << "*****        Sorting Practice       *****" << endl ;
@@ -232,31 +234,31 @@ int main() {
         cout << "***** 3 : Radix Sort                *****" << endl ;
         cout << "*****************************************" << endl ;
         cout << endl << "Please enter your choice :" << endl ;
-        
+
         cin >> command ; // read in user command
-        
+
         if ( command == 0 ) { // bye :(((
             cout << "Bye :(((" << endl ;
             return 0 ;
         } // quit
-        
+
         else if ( command > 3 || command < 0 ) { // wrong command
             cout << "Error command! please enter an acceptable command :" << endl << endl ;
             continueOrNot = true ;
         } // wrong command
-        
+
         else if ( command == 1 ) { // function 1
             bool function1Confirm = false ;
-            
+
             do {
                 cout << "Please enter the file you want to read and copy or [0] to quit:" << endl ;
                 cin >> FileN ;
-                
+
                 if ( FileN == "0" ) {
                     function1Confirm = true ;
                     continueOrNot = true ;
                 } // quit
-                
+
                 else {
                     string fileName = "input" + FileN + ".txt" ;
                     input.open( fileName.c_str() ) ;
@@ -292,22 +294,22 @@ int main() {
                     else cout << "*****  " << fileName << " does not exist!  *****" << endl ; // no file
                 } // open and sort
             } while( ! function1Confirm ) ;
-            
+
             FileN = "0" ;
         } // mission 1: Select & Bubble Sort
-        
+
         else if ( command == 2 ) { // function 2
             bool function2Confirm = false ;
-            
+
             do {
                 cout << "Please enter the file you want to read and copy or [0] to quit:" << endl ;
                 cin >> FileN ;
-                
+
                 if ( FileN == "0" ) {
                     function2Confirm = true ;
                     continueOrNot = true ;
                 } // quit
-                
+
                 else {
                     string fileName = "input" + FileN + ".txt" ;
                     input.open( fileName.c_str() ) ;
@@ -345,22 +347,22 @@ int main() {
                     else cout << "*****  " << fileName << " does not exist!  *****" << endl ; // no file
                 } // open and sort
             } while( ! function2Confirm ) ;
-            
+
             FileN = "0" ;
         } // mission 2: Merge & Quick Sort
-        
+
         else if ( command == 3 ) { // function 3
             bool function3Confirm = false ;
-            
+
             do {
                 cout << "Please enter the file you want to read and copy or [0] to quit:" << endl ;
                 cin >> FileN ;
-                
+
                 if ( FileN == "0" ) {
                     function3Confirm = true ;
                     continueOrNot = true ;
                 } // quit
-                
+
                 else {
                     string fileName = "input" + FileN + ".txt" ;
                     input.open( fileName.c_str() ) ;
@@ -380,7 +382,7 @@ int main() {
                     else cout << "*****  input" << FileN << ".txt does not exist!  *****" << endl ; // no file
                 } // open and sort
             } while( ! function3Confirm ) ;
-            
+
             Count = 0 ;
             FileN = "0" ;
             input.close() ;
